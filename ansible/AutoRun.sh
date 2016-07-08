f56544ba1d6cc1c78459a3118bbbe1a731c78f8c
Bin Liu <bin.w.liu@gmail.com>
2016-07-07 22:18:46 -0400

    Add AutoRun script to run ansible with tags
    Add AutoRun script to run ansible with tags
    Add -v mongo option to mongo role

---
+++ ansible/AutoRun.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+then 
+    echo "Default Option: Running All Rolls"
+    ansible-playbook -i hosts -vvv services.yml
+elif [ $1 = "-h" ];
+then
+    echo "This script is to run ansible with express option"
+    echo " -exp : run Ansible with all the tasks with tag 'infrequent' excluded"
+elif [ $1 = "-exp" ]; 
+then
+    echo "Express Option: Running Rolls Excluding Tag=Infrequent"
+    ansible-playbook --skip-tags "infrequent" -i hosts -vvv services.yml
+else
+    echo "Unrecognized Option: Please use -h for help"
+fi
