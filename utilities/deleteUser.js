527ba20e6c04d9c042297ee690d11818e04f7476
desongyu <desong.e.yu@gmail.com>
2016-07-27 15:26:26 -0400

    completed v1 of deleteUser

--- utilities/deleteUser.js
+++ utilities/deleteUser.js
@@ -6,14 +6,13 @@ var yaml = require('js-yaml');
 
 
 var yesHandlerToSet = null
+var fileContent = [];
 
 var replServer = repl.start({prompt: '> ', useColors: true, writer: function (x) {
-    console.log("-----------repl start writer----------")	
 	return x
 }});
 
 replServer.defineCommand('y', function() {
-    console.log("----------defineCommand y----------")
 	if (yesHandlerToSet) {
 		yesHandlerToSet()
 	} else {
@@ -27,14 +26,7 @@ replServer.defineCommand('y', function() {
 
 /////////////////////////////////////////////////////////////
 // define commands here
-
-replServer.context.del = function (fileName) {
-    console.log("-----------context.del----------")
-  	yesHandlerToSet = function () {      
-        
-  		console.log("deleted everything in " + fileName);
-  	}
-    
+queryUser = function(fileName){
     mongodb.MongoClient.connect(process.env.MONGODB_URL || "mongodb://localhost/lynedup", function (error, db) {
         if (error) {
             console.log(error)
@@ -42,27 +34,64 @@ replServer.context.del = function (fileName) {
         }
 
         // Read in file content
-        var fileContentTmp = fs.readFileSync(fileName).toString().split("\n");
-        var fileContent = [];
-        
+        var fileContentTmp = fs.readFileSync(fileName).toString().split("\n");        
+               
         for (i in fileContentTmp) {            
-            // Remove any line feeds
-            fileContent[i] = fileContentTmp[i].replace(/\n|\r/g,"");
-            console.log("\n"+JSON.stringify(fileContent[i])+"!");        
-        
+            // Convert content to lower case and remove white spaces
+            fileContent[i] = fileContentTmp[i].toLowerCase().trim();            
         }
-        
-        //fileContent = fileContentTmp.map(function(x){return x.toLowerCase().trim()})
-        
-        var cond={$regex: fileContent, $options: "i"};
-         db.collection('users').find({emailAddress: {$in:
-            fileContent}}).toArray(function (error, results) {
-             console.log(results);
-        })
-        
-        console.log("finding ---- " + JSON.stringify(fileContent[0]) + "!")
-        
-         console.log("are you sure?? enter .y")
-        return ""
+     
+        // Query on the Users collection for matching email addresses
+        db.collection('users').find(
+            {
+                emailAddress: { $in: fileContent }
+            })
+            .toArray(function (error, results) {
+            
+             console.log("\nRequested to delete: " + fileContent.length);
+             console.log("Found in DB: " + results.length);
+             console.log("Are you sure?? enter .y")
+        })   
+})
+}
+  
+deleteUser = function() {
+    mongodb.MongoClient.connect(process.env.MONGODB_URL || "mongodb://localhost/lynedup", function (error, db) {      
+
+        if (error) {
+            console.log(error)
+            process.exit(0)            
+        }
+
+        try {            
+            db.collection('users').deleteMany(
+                {
+                    emailAddress: {$in: fileContent}
+                }, 
+                function(err, r){
+                    
+                    if (error) {
+                        console.log(error)
+                        process.exit(0)            
+                    }
+                    
+                    console.log("Deleted " + r.deletedCount);
+                });
+            
+        } catch (e) {
+            console.log(e)
+        }           
+
     })
+}
+
+
+replServer.context.del = function (fileName) {
+  	yesHandlerToSet = function () {        
+      deleteUser();        
+    }
+    
+    queryUser(fileName);          
+        
+    return ""
 }
