ab970d21e1ac1c3985d1abbdec4b8fbfecaa134c
desongyu <desong.e.yu@gmail.com>
2016-07-29 15:38:12 -0400

    incorporate dennis comment
    (1) instead of just printing out the number of email addresses in the
    original input file and the number of accounts found, please print out
    all the email addresses that could not be matched.
    (2) for the ones that do match, save the objects into a separate array
    so that if “.y” is entered, the array of user account JSON objects can
    be written to disk (a kind of backup log in case something goes wrong).
    (3) instead of passing the emailAddress’s into deleteMany(), i’d like
    you to use the _id’s from the results of the initial find() call. that
    is a lot safer and protects against the theoretical possibility that one
    of the email addresses doesn’t originally match but before you press .y,
    a new user registers with that email address and then is subsequently
    deleted even though the administrator was never told that the account
    would be deleted.

--- utilities/deleteUser.js
+++ utilities/deleteUser.js
@@ -7,6 +7,8 @@ var yaml = require('js-yaml');
 
 var yesHandlerToSet = null
 var fileContent = [];
+var foundDocuments = [];
+var foundEmailIDs = [];
 
 var replServer = repl.start({prompt: '> ', useColors: true, writer: function (x) {
 	return x
@@ -25,9 +27,9 @@ replServer.defineCommand('y', function() {
 })
 
 queryUser = function(fileName){
-    mongodb.MongoClient.connect(process.env.MONGODB_URL || "mongodb://localhost/lynedup", function (error, db) {
-        if (error) {
-            console.log(error)
+    mongodb.MongoClient.connect(process.env.MONGODB_URL || "mongodb://localhost/lynedup", function (err, db) {
+        if (err) {
+            console.log(err)
             process.exit(0)            
         }
 
@@ -38,50 +40,72 @@ queryUser = function(fileName){
         fileContent = fileContentTmp.map(function(x){return x.toLowerCase().trim();});
              
         // Query on the Users collection for matching email addresses
-        db.collection('users').find(
-            {
-                emailAddress: { $in: fileContent }
-            })
-            .toArray(function (error, results) {
+        db.collection('users').find({
 
-            if (error) {
-                console.log(error)
+            emailAddress: { $in: fileContent }
+
+        }).toArray(function (err, found) {
+
+            if (err) {
+                console.log(err)
                 process.exit(0)            
             }
-            
-             console.log("\nRequested to delete: " + fileContent.length);
-             console.log("Found in DB: " + results.length);
-             console.log("Are you sure?? enter .y")
+
+            // Find only the emailAddress property from the found emails
+            var foundEmails = found.map(function(x) {return x.emailAddress;});                    
+            // Find emails that do not appear in the found emails
+            missing = fileContent.filter(function(x) { return foundEmails.indexOf(x)<0});
+
+            // Save the list of found emails
+            foundDocuments = found;
+
+             // Find only the _id property from the found emails
+            foundEmailIDs = found.map(function(x) {return x._id;});   
+                
+            console.log("\nRequested to delete: " + fileContent.length);
+            console.log("Matched in DB: " + found.length);
+            console.log("Missing in DB: " + missing.length);
+            console.log(yaml.dump(missing));
+            console.log("Are you sure?? enter .y")     
         })   
 })
 }
   
 deleteUser = function() {
-    mongodb.MongoClient.connect(process.env.MONGODB_URL || "mongodb://localhost/lynedup", function (error, db) {      
+    mongodb.MongoClient.connect(process.env.MONGODB_URL || "mongodb://localhost/lynedup", function (err, db) {      
 
-        if (error) {
-            console.log(error)
+        if (err) {
+            console.log(err)
             process.exit(0)            
         }
 
-        try {            
-            db.collection('users').deleteMany(
-                {
-                    emailAddress: {$in: fileContent}
-                }, 
-                function(err, r){
+        // Save the list of matching documents first before try to delete
+        var seconds = new Date().getTime() / 1000;
+        fs.writeFile("foundDocs_"+seconds, JSON.stringify(foundDocuments), function(err){
+            if(err) {
+                console.log("Failed to save matched documents to disk: " + err);
+                process.exit(0);
+            }
+
+            try {            
+                db.collection('users').deleteMany({
+
+                    _id: {$in: foundEmailIDs}
+
+                }, function(err, r){
                     
-                    if (error) {
-                        console.log(error)
+                    if (err) {
+                        console.log(err)
                         process.exit(0)            
                     }
                     
                     console.log("Deleted " + r.deletedCount);
                 });
-            
-        } catch (e) {
-            console.log(e)
-        }           
+            } catch (e) {
+                console.log(e)
+            }          
+
+        })      
 
     })
 }
